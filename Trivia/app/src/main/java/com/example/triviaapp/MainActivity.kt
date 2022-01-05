9a69025e4afc812bcd6d0003e236cf97bb1d4075
My AnFeVaGa <Job.andresvalencia@gmail.com>
2022-01-05 14:10:17 -0500

    The adapter package is added with the DataAdapter class and list_item.xml xml file

--- Trivia/app/src/main/java/com/example/triviaapp/MainActivity.kt
+++ Trivia/app/src/main/java/com/example/triviaapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.triviaapp.databinding.ActivityMainBinding
+import javax.sql.DataSource
 
 class MainActivity : AppCompatActivity() {
 
--- Trivia/app/src/main/java/com/example/triviaapp/adapter/ItemAdapter.kt
+++ Trivia/app/src/main/java/com/example/triviaapp/adapter/ItemAdapter.kt
@@ -2,20 +2,38 @@ package com.example.triviaapp.adapter
 
 import android.content.Context
 import android.service.autofill.Dataset
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.triviaapp.R
 import com.example.triviaapp.model.Player
 
 
-//class ItemAdapter(
+class ItemAdapter(
 //    private val context: Context,
-//    private val dataset: List<Player>
-//) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
-//
-//    inner class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-//        val textView: TextView = view.findViewById(R.id.)
-//    }
-//
-//}
+    private val dataset: List<Player>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textViewName.text = dataset[position].name
+        holder.textViewPosition.text = dataset[position].points.toString()
+    }
+
+    override fun getItemCount() = dataset.size
+
+    inner class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textViewName: TextView = view.findViewById(R.id.name_player)
+        val textViewPosition: TextView = view.findViewById(R.id.points_player)
+    }
+
+}
